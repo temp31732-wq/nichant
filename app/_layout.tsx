b7e1d0094642fdae8856f0bbe0b566d6c86950ab
Pankajkumar7970 <121349902+Pankajkumar7970@users.noreply.github.com>
2025-08-01 11:37:26 +0530

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { useFrameworkReady } from '../hooks/useFrameworkReady';
 
 export default function RootLayout() {
   useFrameworkReady();
